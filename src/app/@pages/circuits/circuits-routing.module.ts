3267921f2678ab12833cc7f72bc80784d3a4470d
Anartz Mugika Ledo <mugan86@gmail.com>
2019-12-26 18:38:08 +0100

    2.- Configurar los ficheros routing de todas las páginas, antes de añadir la configuración principal de las rutas con Lazy Loading

--- src/app/@pages/circuits/circuits-routing.module.ts
+++ src/app/@pages/circuits/circuits-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CircuitsComponent } from './circuits.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: CircuitsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/@pages/drivers/drivers-routing.module.ts
+++ src/app/@pages/drivers/drivers-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DriversComponent } from './drivers.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: DriversComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
