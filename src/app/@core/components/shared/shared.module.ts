a365c12023ac8998ccd07bd7e2a9da64f78ffb92
Anartz Mugika Ledo <mugan86@gmail.com>
2020-01-01 19:42:25 +0100

    10.- Importar módulo GraphQL para usarlo

--- src/app/@core/components/shared/shared.module.ts
+++ src/app/@core/components/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { FooterModule } from './footer/footer.module';
 import { NavbarModule } from './navbar/navbar.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GraphqlModule } from 'src/app/@graphql/modules/graphql.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    GraphqlModule,
     NavbarModule,
     FooterModule
   ],
